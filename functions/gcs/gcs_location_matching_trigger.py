e051c67472e4ca6a9ced38377551f276bb22806f
Danny Viorel <dviorel@inmarket.com>
2020-05-11 09:37:01 -0400

    pass filtered columns to GBQ table

--- functions/gcs/gcs_location_matching_trigger.py
+++ functions/gcs/gcs_location_matching_trigger.py
@@ -10,13 +10,7 @@ bucket = 'location_matching'
 logging.basicConfig(level=logging.INFO)
 
 
-def _verify_fields(columns):
-    # It must have chain name/chain id/sic code/NAICS, address
-    # It could have city/state/zip
-    # required_fields = ['chain name/chain id/sic code/NAICS', 'address']
-    # optional_fields = ['city', 'state', 'zip']
-    validation_fields = {'chain name/chain id/sic code/NAICS': True, 'address': True, 'city': False,
-                         'state': False, 'zip': False}
+def _verify_fields(columns, validation_fields):
     column_validation_fields = []
     for required_field in validation_fields.keys():
         if "/" in required_field:
@@ -29,32 +23,35 @@ def _verify_fields(columns):
                 column_validation_fields.append(token)
                 found = True
                 break
-        if validation_fields[required_field] and not found:
+        if validation_fields[required_field][0] and not found:
             raise Exception(f'Field {required_field} not founded in headers!')
     return column_validation_fields
 
 
 def process_created(data, context):
     try:
+        validation_fields = {'chain name/chain id/sic code/NAICS': (True, 'chain_name'),
+                             'address/address full/address full (no zip)': (True, 'address'),
+                             'city': (False, 'city'), 'state': (False, 'state'), 'zip': (False, 'zip')}
         file_name = data['name']
         if file_name.endswith('/'):
-            print(f'NOPFolder created {file_name}, ignored')
             logging.info(f'Folder created {file_name}, ignored')
             return
         logging.info(f'File: {file_name}')
-        print(f'NOPFile: {file_name}')
         raw_data = pd.read_csv(f'gs://{bucket}/{file_name}', sep=',')
-        column_validation_fields = _verify_fields(raw_data.keys())
-        desired_data = raw_data[column_validation_fields]
-        desired_data.rename(columns=lambda name: name.replace(' ', '_'), inplace=True)
+        column_validation_fields = _verify_fields(raw_data.keys(), validation_fields)
+        selected_columns = raw_data[column_validation_fields].rename(columns=lambda name: name.replace(' ', '_').
+                                                                     replace('(', '_').replace(')', '_'), inplace=False)
+        # Complete columns not present in file
+        for key in validation_fields:
+            if not validation_fields[key][0] and validation_fields[key][1] not in selected_columns:
+                selected_columns[validation_fields[key][1]] = ''
         temp_table = f'tmp_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}'
         logging.info(f'Will write to table: {temp_table}')
-        desired_data.to_gbq(f'{dataset}.{temp_table}', progress_bar=False)
-        print(f'Rows: {len(desired_data.index)}')
+        selected_columns.to_gbq(f'{dataset}.{temp_table}', project_id=project, progress_bar=False)
     except Exception as e:
-        logging.error('Error unexpected')
-        print(f'Unexpected error: {e}')
+        logging.error(f'Unexpected error {e}')
 
 
 # process_created({'name': 'dviorel/Sample_1 updated.csv'}, None)
-process_created({'name': 'dviorel/sample_2_subset.csv'}, None)
+process_created({'name': 'dviorel/sample_2_small_subset_nozip.csv'}, None)
