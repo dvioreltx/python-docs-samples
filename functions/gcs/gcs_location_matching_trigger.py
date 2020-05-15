9cf72fd7b66244be22041c73b9c14fa67ea59117
Danny Viorel <dviorel@inmarket.com>
2020-05-15 19:22:52 -0400

    refactored

--- functions/gcs/gcs_location_matching_trigger.py
+++ functions/gcs/gcs_location_matching_trigger.py
@@ -6,10 +6,8 @@ from google.cloud import bigquery
 from google.cloud import bigquery_storage_v1beta1
 
 dataset = "dannyv"
-table = "test_02"
 project = "cptsrewards-hrd"
 bucket = 'location_matching'
-
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -53,7 +51,6 @@ def _get_state_code(state, df_states):
     match = df_states[df_states['state_name'].str.strip().str.lower() == state.strip().lower()]
     if len(match) == 0:
         return ' '
-    #    raise Exception(f'No abbreviation for state {state}!')
     if len(match) > 1:
         raise Exception(f'{len(match)} occurrences abbreviation for state {state}!')
     return match.iloc[0]['state_abbr']
@@ -68,59 +65,62 @@ def _get_chain_name(chain_id, df_chains):
     return match.iloc[0]['name']
 
 
+def _verify_match_df(df_match, possible_value):
+    if len(df_match.index) > 0:
+        return True, possible_value
+    return False, None
+
+
+def _verify_match(value_1_return, value_2):
+    if value_1_return.lower() == value_2.lower():
+        return True, value_1_return
+    return False, None
+
+
 def _split_address_data(address_full, df_states, df_cities, include_zip, first_state):
-    # remove commas
     address_full = address_full.replace(',', ' ')
-    # if address_full == '6545 TRIGO RD ISLA VISTA':
-    if '2400 ALABAMA ST BELLINGHAM WA 98229' in address_full:
-        stop = True
     tokens = address_full.split(' ')
     tokens = list(filter(None, tokens))
     length = len(tokens)
     if length < 4:
         raise Exception(f'Just {length} tokens founded for {address_full}, waiting 4 at less')
-    if include_zip:
-        zip_code = tokens[len(tokens) - 1]
-    else:
-        zip_code = None
-    # look for state
-    found = False
-    state = ''
-
-    if not first_state:
-        state_position = len(tokens) - (1 if include_zip else 2) - 1
-        expected_match = df_states[df_states['state_abbr'].str.upper() == tokens[state_position].upper()]
-        if len(expected_match.index) > 0:
-            state = tokens[state_position]
-            found = True
-        if not found:
-            expected_match = df_states[df_states['state_name'].str.upper() == tokens[state_position].upper()]
-            if len(expected_match.index) > 0:
-                state = tokens[state_position]
-                found = True
-        if not found:
-            expected_match = df_states[df_states['state_name'].str.lower() == tokens[state_position - 1].lower()
-                                       + ' ' + tokens[state_position].lower()]
-            if len(expected_match.index) > 0:
-                state = tokens[state_position - 1] + ' ' + tokens[state_position]
-                found = True
-    else:
-        state_position = len(tokens) - (2 if include_zip else 3) - 1
-        expected_match = df_states[df_states['state_abbr'].str.upper() == tokens[state_position].upper()]
-        if len(expected_match.index) > 0:
-            state = tokens[state_position]
-            found = True
-        if not found:
-            expected_match = df_states[df_states['state_name'].str.upper() == tokens[state_position].upper()]
-            if len(expected_match.index) > 0:
-                state = tokens[state_position]
-                found = True
-        if not found:
-            expected_match = df_states[df_states['state_name'].str.lower() == tokens[state_position - 1].lower()
-                                       + ' ' + tokens[state_position].lower()]
-            if len(expected_match.index) > 0:
-                state = tokens[state_position - 1] + ' ' + tokens[state_position]
-                found = True
+    zip_code = tokens[len(tokens) - 1] if include_zip else None
+    state_position = len(tokens) - (1 if include_zip else 2) - 1 - (1 if first_state else 0)
+    found, state = _verify_match_df(df_states[df_states['state_abbr'] == tokens[state_position].upper()],
+                                    tokens[state_position])
+    if not found and first_state:
+        found, state = _verify_match_df(df_states[df_states['state_abbr'] == tokens[state_position - 1].upper()],
+                                        tokens[state_position - 1])
+    if not found:
+        found, state = _verify_match_df(df_states[df_states['state_name'].str.lower() == tokens[state_position].lower()]
+                                        , tokens[state_position])
+    if not found and first_state:
+        found, state = _verify_match_df(df_states[df_states['state_name'].str.lower() ==
+                                                  tokens[state_position - 1].lower()], tokens[state_position - 1])
+    if not found:
+        found, state = _verify_match_df(df_states[df_states['state_name'].str.lower() == tokens[state_position - 1]
+                                        .lower() + ' ' + tokens[state_position].lower()], tokens[state_position - 1] +
+                                        ' ' + tokens[state_position])
+    if not found:
+        for index, row in df_states.iterrows():
+            state_position = len(tokens) - (1 if include_zip else 2) - 1
+            state, found = _verify_match(tokens[state_position], row['state_abbr'])
+            if not found and first_state:
+                state, found = _verify_match(tokens[state_position - 1], row['state_abbr'])
+            if not found:
+                state, found = _verify_match(tokens[state_position], row['state_name'])
+            if not found and first_state:
+                state, found = _verify_match(tokens[state_position - 1], row['state_name'])
+            if not found:
+                state, found = _verify_match(tokens[state_position - 1] + ' ' + tokens[state_position],
+                                             row['state_abbr'])
+            if not found:
+                if row['state_name'].lower() in address_full.lower():
+                    position = address_full.lower().rfind(row['state_name'].lower())
+                    state = address_full[position:position + len(row['state_name'])]
+                    found = True
+            if found:
+                break
     if found:
         if first_state:
             if include_zip:
@@ -174,100 +174,7 @@ def _split_address_data(address_full, df_states, df_cities, include_zip, first_s
             #             city = address_full[position:position + len(row_city['city'])]
             #             address = address_full[:position]
             #             break
-    if not found:
-        for index, row in df_states.iterrows():
-            if not first_state:
-                state_position = len(tokens) - (1 if include_zip else 2) - 1
-                if row['state_abbr'] == 'WA':
-                    stop_this = 1
-                if len(tokens[state_position]) == 2 and tokens[state_position].lower() == row['state_abbr'].lower():
-                    state = row['state_abbr']
-                    found = True
-                if tokens[state_position].lower() == row['state_name'].lower():
-                    state = row['state_name']
-                    found = True
-                if tokens[state_position - 1].lower() + ' ' + tokens[state_position].lower() == \
-                        row['state_name'].lower():
-                    state = row['state_name']
-                    found = True
-            else:
-                state_position = len(tokens) - (2 if include_zip else 3) - 1
-                if len(tokens[state_position]) == 2 and tokens[state_position].upper() == row['state_abbr'].upper():
-                    state = row['state_abbr']
-                    found = True
-                if len(tokens[state_position - 1]) == 2 and tokens[state_position - 1].upper() == row['state_abbr'].upper():
-                    state = row['state_abbr']
-                    found = True
-                if tokens[state_position].lower() == row['state_name'].lower():
-                    state = row['state_name']
-                    found = True
-                if tokens[state_position - 1].lower() == row['state_name'].lower():
-                    state = row['state_name']
-                    found = True
-                if tokens[state_position - 1].lower() + ' ' + tokens[state_position].lower() == \
-                        row['state_name'].lower():
-                    state = row['state_name']
-                    found = True
-            if not found:
-                if row['state_name'].lower() in address_full.lower():
-                    position = address_full.lower().rfind(row['state_name'].lower())
-                    # state = row['state_name']
-                    state = address_full[position:position + len(row['state_name'])]
-                    found = True
-            if found:
-                if first_state:
-                    if include_zip:
-                        city = address_full[address_full.rfind(state) + len(state):address_full.rfind(' ')]
-                    else:
-                        city = address_full[address_full.rfind(state) + len(state):]
-                    address = address_full[:address_full.rfind(city)]
-                    state = _get_state_code(state, df_states)
-                else:
-                    # try city with the previous token
-                    city_found = False
-                    position = address_full.rfind(state)
-                    sub_address = address_full[:position]
-                    sub_address_tokens = sub_address.split(' ')
-                    sub_address_tokens = list(filter(None, sub_address_tokens))
-                    city = sub_address_tokens[len(sub_address_tokens) - 1]
-                    state = _get_state_code(state, df_states)
-                    address = sub_address[:sub_address.rfind(' ')]
-                    filtered_cities = df_cities[df_cities['state'] == state]
 
-                    # 3 chars
-                    if not city_found and len(sub_address_tokens) > 4:
-                        expected_city = sub_address_tokens[len(sub_address_tokens) - 3] + ' ' + \
-                                        sub_address_tokens[len(sub_address_tokens) - 2] + ' ' + \
-                                        sub_address_tokens[len(sub_address_tokens) - 1]
-                        expected_match = filtered_cities[filtered_cities['city'].str.lower() == expected_city.lower()]
-                        if len(expected_match.index) > 0:
-                            city = expected_city
-                            address = sub_address[:sub_address.rfind(expected_city)]
-                            city_found = True
-                    # 2 chars
-                    if not city_found and len(sub_address_tokens) > 3:
-                        expected_city = sub_address_tokens[len(sub_address_tokens) - 2] + ' ' + \
-                                        sub_address_tokens[len(sub_address_tokens) - 1]
-                        expected_match = filtered_cities[filtered_cities['city'].str.lower() == expected_city.lower()]
-                        if len(expected_match.index) > 0:
-                            city = expected_city
-                            address = sub_address[:sub_address.rfind(expected_city)]
-                            city_found = True
-                    # 1 char
-                    if not city_found:
-                        expected_match = filtered_cities[filtered_cities['city'].str.lower() == city.lower()]
-                        if len(expected_match.index) > 0:
-                            address = sub_address[:sub_address.rfind(city)]
-                            city_found = True
-
-                    # if not city_found:
-                    #     for index_city, row_city in filtered_cities.iterrows():
-                    #         if row_city['city'] is not None and row_city['city'].lower() in address_full.lower():
-                    #             position = address_full.lower().rfind(row_city['city'].lower())
-                    #             city = address_full[position:position + len(row_city['city'])]
-                    #             address = address_full[:position]
-                    #             break
-                break
     if not found:
         raise Exception(f'No data found for {address_full}')
     return address, state, city, zip_code
@@ -276,7 +183,7 @@ def _split_address_data(address_full, df_states, df_cities, include_zip, first_s
 def process_created(data, context):
     try:
         logging.debug(f'Started {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-        validation_fields = {'chain/chain id/sic code/naics': (True, 'chain_name'),
+        validation_fields = {'sic code': (False, 'sic_code'), 'chain name/chain id': (True, 'chain'),
                              'address/address full/address full (no zip)': (False, 'address'),
                              'city': (False, 'city'), 'state': (False, 'state'), 'zip': (False, 'zip')}
         file_name = data['name']
@@ -284,7 +191,6 @@ def process_created(data, context):
             logging.info(f'Folder created {file_name}, ignored')
             return
         logging.info(f'File: {file_name}')
-        # raw_data = pd.read_csv(f'gs://{bucket}/{file_name}', sep=',')
         try:
             raw_data = pd.read_csv(f'gs://{bucket}/{file_name}', sep='\t')
         except ValueError:
@@ -307,7 +213,7 @@ def process_created(data, context):
         # Complete columns not present in file
         for key in validation_fields:
             if not validation_fields[key][0] and validation_fields[key][1] not in selected_columns:
-                selected_columns[validation_fields[key][1]] = ''
+                selected_columns[validation_fields[key][1]] = None
         if 'chain_id' in selected_columns.columns:
             df_chains = (bq_client.query('SELECT chain_id, name, sic_code from `inmarket-archive`.scansense.chain').
                          result().to_dataframe(bqstorage_client=bq_storage_client))
@@ -348,18 +254,26 @@ def process_created(data, context):
                                                                     _get_state_code(state_lambda, df_states))
         selected_columns.rename(columns={'chain': 'chain_name', 'address': 'street_address'}, inplace=True)
         selected_columns['category'] = None
-        # selected_columns['sic_code'] = None
         selected_columns['lat'] = None
         selected_columns['lon'] = None
         if 'address_full' in selected_columns.columns:
             selected_columns = selected_columns.drop(['address_full'], axis=1)
-        if 'sic_code' not in selected_columns.columns:
-            selected_columns['sic_code'] = None
-        # selected_columns = selected_columns.drop(['address_full'], axis=1)
-        temp_table = f'tmp_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}'
-        # temp_table = f'sic_code_match'
+        # temp_table = f'tmp{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}'
+        temp_table = f'store_list_full_address'
         logging.info(f'Will write to table: {temp_table}')
         selected_columns.to_gbq(f'{dataset}.{temp_table}', project_id=project, progress_bar=False)
+        logging.debug(f'will add new fields: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        add_clean_fields = f""" CREATE TEMP FUNCTION cleanStr(str string, type string) RETURNS string
+                              LANGUAGE js AS \"\"\"
+                                return cleanStr(str, type)
+                            \"\"\"
+                            OPTIONS (library=['gs://javascript_lib/addr_functions.js']);
+                            create or replace table {dataset}.{temp_table} as 
+                            select *, cleanstr(chain_name, 'chain') clean_chain, 
+                            cleanstr(street_address, 'addr') clean_addr
+                            from `{dataset}.{temp_table}`  
+        """
+        bq_client.query(add_clean_fields, project=project)
         logging.debug(f'Finished {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
     except Exception as e:
         logging.exception(f'Unexpected error {e}')
@@ -373,5 +287,12 @@ def process_created(data, context):
 # process_created({'name': 'dviorel/walmart_match_issue.txt'}, None)
 # process_created({'name': 'dviorel/store_full_address.txt'}, None)
 # process_created({'name': 'dviorel/simple_list.txt'}, None)
-process_created({'name': 'dviorel/sic_code_match.txt'}, None)
-# process_created({'name': 'dviorel/match_multiple_sic_codes.txt'}, None)
+#  process_created({'name': 'dviorel/sic_code_match.txt'}, None)
+# process_created({'name': 'dviorel/match_multiple_sic_codes.txt'}, None)      # No chain names
+# process_created({'name': 'dviorel/chain_id_name_both.txt'}, None) # Error encoding
+# process_created({'name': 'dviorel/multiple_chain_ids.txt'}, None) # no chain id for list
+# process_created({'name': 'dviorel/sample_1_updated.txt'}, None) OK
+# process_created({'name': 'dviorel/sample_2.txt'}, None) OK
+# process_created({'name': 'dviorel/store_list_with_only_zip_code.txt'}, None) OK
+# process_created({'name': 'dviorel/unclean_list.txt'}, None)
+process_created({'name': 'dviorel/store_list_full_address.txt'}, None)
