d22fc740b56a3f12d304cff93e21c1b9becde713
Danny Viorel <dviorel@inmarket.com>
2020-06-22 22:05:31 -0400

    Log cleaned for pre_process_file

--- functions/gcs/gcs_location_matching_created.py
+++ functions/gcs/gcs_location_matching_created.py
@@ -18,9 +18,7 @@ default_args = {
     'depends_on_past': False,
     'email': ['dviorel@inmarket.com'],
     'email_on_failure': True,
-    'email_on_retry': True,
     'retries': 0,
-    'retry_delay': datetime.timedelta(minutes=5),
     'start_date': datetime.datetime(2020, 6, 1)
 }
 
@@ -40,7 +38,7 @@ data_set_final = "location_matching_match"
 bucket = 'location_matching'
 expiration_days_original_table = 7
 expiration_days_results_table = 30
-block_size = 2000
+block_size = 3000
 project = "cptsrewards-hrd"
 url_auth_gcp = 'https://www.googleapis.com/auth/cloud-platform'
 logging.basicConfig(level=logging.DEBUG)
@@ -51,7 +49,6 @@ query_cities = 'select distinct city, state from (select distinct city, state fr
 
 
 class LMAlgo(Enum):
-    # CHAIN = 1
     CHAIN_ZIP = 1
     CHAIN_CITY = 2
     SIC_CODE = 3
@@ -952,17 +949,16 @@ def delete_temp_data(**context):
 
 def pre_process_file(**context):
     try:
-        logging.debug(f'Started {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        original_name = context['dag_run'].conf['original_file_name']
+        logging.info(f'Started for {original_name}')
         validation_fields = {'sic code': 'sic_code', 'chain name/chain id': 'chain_name',
                              'address/address full/address full (no zip)/address full (address, state, city, zip)':
                                  'address', 'city': 'city', 'state': 'state', 'zip': 'zip'}
-        original_name = context['dag_run'].conf['original_file_name']
         file_full_name = original_name.replace(' ', '_').replace('-', '_')
         destination_email = context['dag_run'].conf['destination_email']
         file_name = context['dag_run'].conf['the_file_name']
         cf_name = original_name[original_name.index('/') + 1:]
         now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        logging.info(f'File created: {original_name}')
         try:
             raw_data = pd.read_csv(f'gs://{bucket}/{original_name}', sep='\t', encoding='utf-8')
         except Exception:
@@ -975,9 +971,7 @@ def pre_process_file(**context):
                            f'"{cf_name}" is not a valid supported file type. Please verify the file '
                            f'format is "Tab delimited Text(.txt)" before resubmitting for matching.')
                 return
-        # credentials, your_project_id = google.auth.default(scopes=[url_auth_gcp])
-        # bq_client = bigquery.Client(project=project, credentials=credentials)
-        # logging.warning(f'Will write raw table: {file_name}')
+        logging.info('Data read correctly')
         raw_data.columns = map(str.lower, raw_data.columns)
         raw_data.columns = map(str.strip, raw_data.columns)
         column_validation_fields = _verify_fields(raw_data.keys(), validation_fields)
@@ -991,26 +985,15 @@ def pre_process_file(**context):
                        f'File "{cf_name}" must contain at least one of the following required fields - chain id, '
                        f'chain name or sic code. Please add the required field before resubmitting for matching.')
             return
-        # df_states = (bq_client.query(query_states).result().to_dataframe(bqstorage_client=bq_storage_client))
         df_states = pd.read_gbq(query_states, project_id=project, dialect='standard')
-        # Complete columns not present in file
         should_add_state_from_zip = 'state' not in pre_processed_data.columns and 'zip' in pre_processed_data.columns
         has_chain = 'chain_id' in pre_processed_data.columns or 'chain_name' in pre_processed_data.columns
-        has_sic_code = 'sic_code' in pre_processed_data.columns
-        has_chain_id = 'chain id' in raw_data.columns
-        has_chain_name = 'chain name' in raw_data.columns
-        has_address = 'address' in raw_data.columns
-        has_address_full = 'address full' in raw_data.columns
-        has_address_full_no_zip = 'address full (no zip)' in raw_data.columns
         has_city = 'city' in raw_data.columns
-        has_state = 'state' in raw_data.columns
         has_zip = 'zip' in raw_data.columns
-        # has_sic_code = has_sic_code and not has_chain
-        logging.debug(f'has_sic_code: {has_sic_code} ...{original_name}')
         credentials, _ = google.auth.default(scopes=[url_auth_gcp])
         bq_client = bigquery.Client(project=project, credentials=credentials)
+        # Complete columns not present in file
         if 'chain_id' in pre_processed_data.columns:
-            # df_chains = (bq_client.query(query_chains).result().to_dataframe(bqstorage_client=bq_storage_client))
             df_chains = pd.read_gbq(query_chains, project_id=project, dialect='standard')
             pre_processed_data['chain_name'] = pre_processed_data['chain_id'].apply(
                 lambda chain_id: _get_chain_name(chain_id, df_chains))
@@ -1018,26 +1001,25 @@ def pre_process_file(**context):
             if validation_fields[key] not in pre_processed_data:
                 pre_processed_data[validation_fields[key]] = None
         pre_processed_data = pre_processed_data.astype({'zip': 'str'})
+        logging.info('Columns not present were added')
         if 'address_full__no_zip_' in pre_processed_data.columns or 'address_full' in pre_processed_data.columns \
                 or 'address_full__address_state_city_zip_' in pre_processed_data.columns \
                 or 'address_full__address__state__city__zip_' in pre_processed_data.columns:
+            logging.info('It will pre-process address data')
             has_city = True
-            has_state = True
             if 'address_full' in pre_processed_data.columns or \
                     'address_full__address_state_city_zip_' in pre_processed_data.columns or \
                     'address_full__address__state__city__zip_' in pre_processed_data.columns:
                 has_zip = True
-            # df_cities = (bq_client.query(query_cities).result().to_dataframe(bqstorage_client=bq_storage_client))
             df_cities = pd.read_gbq(query_cities, project_id=project, dialect='standard')
-            logging.debug(f'Cities readed ...{original_name}')
             address = None
             state = None
             city = None
             zip_code = None
-            logging.debug(f'Will processs {len(pre_processed_data.index)} rows ...{original_name}')
+            logging.info(f'It will pre-processs {len(pre_processed_data.index)} rows')
             for index, row in pre_processed_data.iterrows():
                 if index % 500 == 0:
-                    logging.debug(f'Row {index} of {len(pre_processed_data.index)} ...{original_name}')
+                    logging.info(f'Row {index} of {len(pre_processed_data.index)}')
                 if 'address_full__no_zip_' in pre_processed_data.columns:
                     address, state, city, zip_code = _split_address_data(row['address_full__no_zip_'], df_states,
                                                                          df_cities, False, False)
@@ -1065,8 +1047,9 @@ def pre_process_file(**context):
         if 'address_full' in pre_processed_data.columns:
             pre_processed_data = pre_processed_data.drop(['address_full'], axis=1)
         pre_processed_data.insert(0, 'store_id', range(1, 1 + len(pre_processed_data)))
+        logging.info('Data already pre-processed')
         preprocessed_table = file_name.lower()
-        logging.warning(f'Will write to table: {preprocessed_table} ...{original_name}')
+        logging.info(f'It will write to table: {preprocessed_table}')
         if has_chain and not has_zip and not has_city:
             _send_mail(destination_email, f'Missing required fields error in Location Matching Tool for “{cf_name}”',
                        f'File {cf_name} must have at least one of the following fields: zip code or city. Please add '
@@ -1075,39 +1058,11 @@ def pre_process_file(**context):
         pre_processed_data.to_gbq(f'{data_set_original}.{preprocessed_table}', project_id=project, progress_bar=False,
                                   if_exists='replace')
         _set_table_expiration(data_set_original, preprocessed_table, expiration_days_original_table, bq_client)
-        logging.warning(f'Will add clean fields: {preprocessed_table} ...{original_name}')
+        logging.warning(f'It will add clean fields to: {preprocessed_table}')
         _add_clean_fields(preprocessed_table, bq_client)
-        has_multiple_chain_id = False
-        has_multiple_chain_name = False
-        if 'chain id' in raw_data.columns:
-            # queried_df = raw_data[raw_data['chain id'].str.contains(',', na=False)]
-            queried_df = raw_data[raw_data['chain id'].astype(str).str.contains(',', na=False)]
-            if len(queried_df.index) > 0:
-                has_multiple_chain_id = True
-        if 'chain name' in raw_data.columns:
-            # queried_df = raw_data[raw_data['chain name'].str.contains(';', na=False)]
-            queried_df = raw_data[raw_data['chain name'].astype(str).str.contains(';', na=False)]
-            if len(queried_df.index) > 0:
-                has_multiple_chain_name = True
         if should_add_state_from_zip:
-            logging.warning(f'Will add states from zip codes: {preprocessed_table} ...{original_name}')
+            logging.info(f'It will add states from zip codes: {preprocessed_table}')
             _add_state_from_zip(preprocessed_table, bq_client)
-        context['dag_run'].conf['table'] = preprocessed_table
-        context['dag_run'].conf['destination_email'] = destination_email
-        context['dag_run'].conf['has_sic_code'] = has_sic_code
-        context['dag_run'].conf['has_chain'] = has_chain
-        context['dag_run'].conf['original_file_name'] = original_name
-        context['dag_run'].conf['has_chain_id'] = has_chain_id
-        context['dag_run'].conf['has_chain_name'] = has_chain_name
-        context['dag_run'].conf['has_address'] = has_address
-        context['dag_run'].conf['has_address_full'] = has_address_full
-        context['dag_run'].conf['has_address_full_no_zip'] = has_address_full_no_zip
-        context['dag_run'].conf['has_city'] = has_city
-        context['dag_run'].conf['has_state'] = has_state
-        context['dag_run'].conf['has_zip'] = has_zip
-        context['dag_run'].conf['has_multiple_chain_id'] = has_multiple_chain_id
-        context['dag_run'].conf['has_multiple_chain_name'] = has_multiple_chain_name
-        context['dag_run'].conf['file_name'] = cf_name
         storage_client = storage.Client()
         if delete_gcs_files and '___no_mv_gcs' not in file_full_name:
             source_bucket = storage_client.get_bucket(f'{bucket}')
@@ -1118,8 +1073,7 @@ def pre_process_file(**context):
             source_bucket.copy_blob(from_blob, source_bucket,
                                     new_name=f'processed/{destination_email}/{now}_{file_name}.txt')
             source_bucket.delete_blob(from_blob.name)
-        logging.warning(f'Process finished for {file_full_name}')
-
+        logging.warning(f'Pre-process finished for {file_full_name}')
     except Exception as e:
         logging.exception(f'Error with {e} and this traceback: {traceback.format_exc()}')
         _notify_error_adops(context, context['dag_run'].conf['destination_email'], context['dag_run'].conf['file_name'])
